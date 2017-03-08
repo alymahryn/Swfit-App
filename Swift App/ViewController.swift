961403f8ed9977136e2f7b6137bfd4be5b3bba7a
Aly Mahryn <alymahryn@Alys-MacBook-Pro.local>
2017-03-08 21:22:46 +0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Aly Mahryn on 3/8/17.
+//  Copyright © 2017 Aly Mahryn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
